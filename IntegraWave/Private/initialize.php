259f9438a027f85cb3b99ebd2df930f4e1143c90
vijay <vijay.gangus@gmail.com>
2017-11-14 16:32:50 -0500

    Updated Initialize.php

--- IntegraWave/Private/initialize.php
+++ IntegraWave/Private/initialize.php
@@ -1,6 +1,6 @@
 <?php
 
-    ob_start(); //Output Buffering is turned on
+    //ob_start(); //Output Buffering is turned on
 
     // Assign file paths to PHP constants
     // __FILE__ returns the current path to this file
@@ -16,8 +16,12 @@
     // * Can set a hardcoded value:
     // * Can dynamically find everything in URL up to "/public"
 
-    //$public_end = strpos($_SERVER['SCRIPT_NAME'], '/IntegraWave') + 12;
-    $public_end = "";
+    if(strpos($_SERVER['SCRIPT_NAME'], '/IntegraWave') > 0){
+        $public_end = strpos($_SERVER['SCRIPT_NAME'], '/IntegraWave') + 12;
+    }
+    else{
+        $public_end = "";
+    }
     $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
     define("WWW_ROOT", $doc_root);
 
