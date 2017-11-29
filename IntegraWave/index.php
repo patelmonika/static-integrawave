cfb29074cd5de7484302764e5437287c139be8b7
vijay <vijay.gangus@gmail.com>
2017-11-29 11:30:15 -0500

    reverted index.php

--- IntegraWave/index.php
+++ IntegraWave/index.php
@@ -177,7 +177,81 @@
         </nav>
     </header>
     <!-- Left side column. contains the logo and sidebar -->
-    <?php include_once "Shared/left-navigation.php"; ?>
+    <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+            <!-- Sidebar Menu -->
+            <ul class="sidebar-menu">
+                <!-- Optionally, you can add icons to the links -->
+                <li class="active"><a href="index.html"><i class="fa fa-dashboard"></i> <span>Home</span></a></li>
+                <li class="treeview">
+                    <a href="index.html"><i class="fa fa-graduation-cap"></i> <span>Admin</span>
+                        <span class="pull-right-container">
+                     <i class="fa fa-angle-left pull-right"></i>
+                     </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li class="treeview"><a href="./admin-module/country.php">Country</a>
+
+                        </li>
+
+                    </ul>
+                </li>
+                <li class="treeview">
+                    <a href="index.html"><i class="fa fa-graduation-cap"></i> <span>Student</span>
+                        <span class="pull-right-container">
+                     <i class="fa fa-angle-left pull-right"></i>
+                     </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li class="treeview"><a href="./StudentModule/category.html">Courses</a>
+                            <ul class="treeview-menu">
+                        <li><a href="../StudentModule/category.html">ASP</a></li>
+                        <li><a href="../StudentModule/category.html">PHP</a></li>
+                            </ul>
+                        </li>
+                        <li class="treeview"><a href="./StudentModule/category.html">Important Dates</a>
+                            <ul class="treeview-menu">
+                                <li><a href="../StudentModule/dates.html">Schedule</a></li>
+                                <li><a href="../StudentModule/dates.html">Fees Deadlines</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="treeview">
+                <a href="index.html"><i class="fa fa-briefcase"></i> <span>Workers</span>
+                    <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+                </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li class="treeview"><a href="resume.html"><span>Resume</span><span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span> </a>
+                        <ul class="treeview-menu">
+                            <li><a href="../workerModule/resumeTemplate.html">Templates</a> </li>
+                            <li><a href="../workerModule/editResume.html">Customize Resume</a> </li>
+                        </ul>
+                    </li>
+                    <li class="treeview"><a href="resume.html"><span>Cover Letter</span><span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span> </a>
+                        <ul class="treeview-menu">
+                            <li><a href="../workerModule/cvTemplate.html">Templates</a> </li>
+                            <li><a href="../workerModule/editCV.html">Customize Cover Letter</a> </li>
+                        </ul>
+                    </li>
+                    <li class="treeview"><a href="../workerModule/rankCity.html">Ranking of Cities </a></li>
+                    <li class="treeview"><a href="../workerModule/marketTrends.html">Job Market Trends </a></li>
+                </ul>
+                </li>
+
+                <li><a href="profile.html"><i class="fa fa-user"></i> <span>Profile</span></a></li>
+            </ul>
+            <!-- /.sidebar-menu -->
+        </section>
+        <!-- /.sidebar -->
+    </aside>
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
