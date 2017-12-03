4aed83efe784c177e4bd928449ac54a01d512476
SukhpreetBedi <31667962+SukhpreetBedi@users.noreply.github.com>
2017-12-03 18:23:43 -0500

    minor changes in design

--- IntegraWave/Shared/left-navigation.php
+++ IntegraWave/Shared/left-navigation.php
@@ -49,7 +49,9 @@
                     </li>
                     <li class="treeview"><a href="<?php echo url_for('/student-module/courses.php'); ?>">Courses & Grades</a>
                     </li>
-                    <li class="treeview"><a href="<?php echo url_for('/student-module/category.php'); ?>">Important Dates</a>
+                    <li class="treeview"><a href="<?php echo url_for('/student-module/category.php'); ?>">Important Dates<span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+                </span></a>
                         <ul class="treeview-menu">
                             <li><a href="<?php echo url_for('/student-module/dates.php'); ?>">Schedule</a></li>
                             <li><a href="<?php echo url_for('/student-module/dates.php'); ?>">Fees Deadlines</a></li>
