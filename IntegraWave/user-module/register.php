3e8799f6c5aa9e78a3061a9cbf6f99c6fd48b353
vijay <vijay.gangus@gmail.com>
2017-12-10 12:13:34 -0500

    Updated the design for error message

--- IntegraWave/user-module/register.php
+++ IntegraWave/user-module/register.php
@@ -35,7 +35,9 @@ if(isset($_POST['action'])) {
                 <div class="panel-body">
                     <?php
                     if(!is_null($error)){
-                        echo "<p class=\"error-message\">$error</p>";
+                        echo "<div class=\"alert alert-danger alert-dismissible\">
+                              <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">×</button>
+                              <h4><i class=\"icon fa fa-ban\"></i> Alert!</h4>$error</div>";
                     }
                     ?>
                     <form role="form" method="post">
