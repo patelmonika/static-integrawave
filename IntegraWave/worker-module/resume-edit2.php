b4f89ec364c643e30f6f9ef2e1436179e81b752f
SukhpreetBedi <31667962+SukhpreetBedi@users.noreply.github.com>
2017-12-17 10:00:24 -0500

    Resumeedi2.php updated

--- IntegraWave/worker-module/resume-edit2.php
+++ IntegraWave/worker-module/resume-edit2.php
@@ -1,9 +1,8 @@
 <?php
 require_once "../Private/functions.php";
 
-include_once "../Shared/header.php";
 
-include_once "../Shared/left-navigation.php";
+
 
 $response = getData('resumecategory',1);
 foreach ($response as $cat){
