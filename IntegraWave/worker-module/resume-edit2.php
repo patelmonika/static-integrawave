c358e65ae765bf2f0e459a8b87d16fcc213ab692
SukhpreetBedi <31667962+SukhpreetBedi@users.noreply.github.com>
2017-12-17 10:42:02 -0500

    Resumeedi2.php updated

--- IntegraWave/worker-module/resume-edit2.php
+++ IntegraWave/worker-module/resume-edit2.php
@@ -5,7 +5,7 @@ require_once "../Private/functions.php";
 
 
 $response = getData('resumecategory',1);
-foreach ($response[0] as $cat){
+foreach ($response[resume_field] as $cat){
     echo $cat[name];
 
 }
