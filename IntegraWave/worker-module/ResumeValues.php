b000c4bd4ee596dc4ce47005cb056a72223162c7
SukhpreetBedi <31667962+SukhpreetBedi@users.noreply.github.com>
2017-12-16 19:08:31 -0500

    ResumeValues.php updated

--- IntegraWave/worker-module/ResumeValues.php
+++ IntegraWave/worker-module/ResumeValues.php
@@ -31,7 +31,7 @@ if(isset($_POST['action'])) {
             <section class="content">
                 <form class="form-horizontal" method="POST">
                     <?php
-                    $response = getData('resumecategory','1');
+                    $response = getData('resumecategory','0');
 
                     foreach ($response['resumecategory'] as $resumecategory) {
                      echo $resumecategory['name'];
