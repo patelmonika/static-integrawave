2b51ac94ce3e9024f38028355203ce8d49ac7609
SukhpreetBedi <31667962+SukhpreetBedi@users.noreply.github.com>
2017-12-16 20:26:07 -0500

    ResumeValues.php updated

--- IntegraWave/worker-module/ResumeValues.php
+++ IntegraWave/worker-module/ResumeValues.php
@@ -33,7 +33,7 @@ if(isset($_POST['action'])) {
                     <?php
                     $response = getData('resumecategory','1');
 
-                    foreach ($response['resumeCategory'] as $cat) {
+                    foreach ($response as $cat) {
                         echo $cat[name];
 
 }
