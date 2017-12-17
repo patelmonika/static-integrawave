db82ac34908055ab701c5b4c776620327c6fcb01
SukhpreetBedi <31667962+SukhpreetBedi@users.noreply.github.com>
2017-12-16 19:28:55 -0500

    ResumeValues.php updated

--- IntegraWave/worker-module/ResumeValues.php
+++ IntegraWave/worker-module/ResumeValues.php
@@ -29,48 +29,19 @@ if(isset($_POST['action'])) {
             </section>
             <!-- Main content -->
             <section class="content">
-                <form class="form-horizontal" method="POST">
+
                     <?php
                     $response = getAll('resumecategory');
 
                     foreach ($response as $cat) {
-                        echo "<div class=\"col-sm-12\">
-                        <input type='hidden' name='scenario' value='1'/>
-                        <div class=\"box\">
-                            <div class=\"box-header with-border\">
-                                <h3 class=\"box-title\">$cat[name]</h3>
-                               
-                            </div>";
-                       /* foreach ($cat['resumefield'] as $resumefield) {
-                            foreach ($resumefield['name'] as $resumefield) {
-                                echo "<div class=\"box-body\">
-                                        <div class=\"form-group\">
-                                            <label class=\"col-sm-3 control-label\">$group[name]</label>
-                                            <div class=\"col-sm-9\">
-                                       ";
-                                echo "$resumefield[name]</br>";
-
-
-                                echo "
-                                    </div>
-                                </div>
-                            </div>";
-                            }*/
-
-
-                            echo "</div>
-                            </div>";
+                      echo  $cat[name];
                         }
+                               
 
                     ?>
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <button type="submit" class="btn btn-primary pull-right" name="submit">Submit</button>
-                            <button type="reset" class="btn btn-link btn-grey pull-right">Cancel</button>
-                        </div>
-                    </div>
 
-                </form>
+
+
             </section>
         </div>
         <!-- /.content -->
