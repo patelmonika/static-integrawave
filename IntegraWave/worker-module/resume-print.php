755715d086f0fb5c8d16e89f7090b6b74ae75085
SukhpreetBedi <31667962+SukhpreetBedi@users.noreply.github.com>
2017-12-17 21:29:52 -0500

    Resume-print.php

--- IntegraWave/worker-module/resume-print.php
+++ IntegraWave/worker-module/resume-print.php
@@ -155,11 +155,9 @@ $response=getData('worker',1);
                     <section class="title">
                         <h1 class="candidate-name name">
                           <?php
-foreach ($response[pivot] as $pivot){
-    foreach($pivot as $val){
-        echo $val[value];
-    }
-}
+var_dump($response);
+
+
                           ?>
 
                           <?php
